9578e58856e48d845d3691d00038055416afc61f
ndevroy <nathan.devroy@optum.com>
2018-06-08 14:42:03 -0500

    Trying to get this damn war to deploy

--- src/main/java/com/example/thisneverends/ThisneverendsApplication.java
+++ src/main/java/com/example/thisneverends/ThisneverendsApplication.java
@@ -1,12 +1,22 @@
 package com.example.thisneverends;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ThisneverendsApplication {
+public class ThisneverendsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThisneverendsApplication.class, args);
 	}
+
 }
